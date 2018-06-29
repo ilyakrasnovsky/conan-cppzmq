065886a90ec5390b0d7862032769c8530326afe7
Ilya Krasnovsky <ilyakrasnovsky@gmail.com>
2018-06-29 18:32:10 -0400

    Update conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -23,7 +23,7 @@ class CppZmqConan(ConanFile):
         self.options['zmq'].encryption = None
     
     def requirements(self):
-        self.requires.add('zmq/4.2.2@ikrasnovsky/stable')
+        self.requires.add('zmq/4.2.2@bincrafters/stable')
 
     def source(self):
         source_url = "https://github.com/zeromq/cppzmq"
